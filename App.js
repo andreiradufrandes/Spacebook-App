88cf5154470bdd3b9323bc6829ef085707d80d5b
andreiradufrandes <19095947@stu.mmu.ac.uk>
2022-02-19 17:32:48 +0000

    Inital navigation
    Initial iteration of the navigation, with a simple stack navigator created

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text,Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+
+// Set up the screen component
+//  the navigation prop is passed in to every screen component (definition) in the native stack navigator
+function HomeScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+
+      {/* navigate to the details screen */}
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+
+
     </View>
   );
 }
+// details screen
+function DetailsScreen({navigation}) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+        title="Go to Home"
+        onPress={() =>navigation.navigate('Home')}
+      />
+        <Button 
+         title="Go back" 
+         onPress={() => navigation.goBack()} 
+        />
+      </View>
+    );
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    // we wrap the whole nagivation in a navigation container 
+    
+    <NavigationContainer>
+        {/* The order in which we put them is the order in which they appear */}
+      <Stack.Navigator>
+          {/* specify a route using the screen component*/}
+        <Stack.Screen
+         name="Home" 
+         component={HomeScreen} 
+         options={{ title: 'Home' }}
+         />
+        <Stack.Screen 
+         name="Details" 
+         component={DetailsScreen} 
+         options={{ title: 'Details Screen ' }}
+         />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
